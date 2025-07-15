30546048d1a06dce0cc033bb1db571a396b93663
cvamyadav <shivam842926.yadav@gmail.com.com>
2025-07-15 18:59:34 +0530

    user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,12 +15,12 @@ import { BlankPage } from './components/BlankPage';
 import CoursesPage from './components/CoursesPage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  apiKey: 'AIzaSyA621pXulqeiLcAf7ZAHEW3yhUw4dxraVs',
+  authDomain: 'loogincode.firebaseapp.com',
+  projectId: 'loogincode',
+  storageBucket: 'loogincode.appspot.com',
+  messagingSenderId: '115444860248',
+  appId: '1:115444860248:web:69a2826c61c7aae358912d'
 };
 
 // Initialize Firebase
